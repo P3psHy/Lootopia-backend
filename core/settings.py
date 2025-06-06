62b6211bc1721a885a994d936c3bca727454cd1b
P3psHy <76128319+P3psHy@users.noreply.github.com>
2025-06-06 12:52:05 +0200

    .

--- core/settings.py
+++ core/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'lootopia.urls'
+ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
     {
@@ -81,7 +81,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'lootopia.wsgi.application'
+WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
