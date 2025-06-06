1d8160f5c8320c0c28ca28d8550174ce1f88a4a8
P3psHy <76128319+P3psHy@users.noreply.github.com>
2025-06-06 15:26:03 +0200

    suppression projet

--- README.md
+++
@@ -1,1 +0,0 @@
-# Lootopia-backend
--- lootopia/app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class AppMainConfig(AppConfig):  # ← nom unique
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app'
