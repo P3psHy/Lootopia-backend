6b4d62b3beb2fb8489b595f0d9fc346c6974f661
P3psHy <76128319+P3psHy@users.noreply.github.com>
2025-06-06 10:38:57 +0200

    changement app

--- lootopia/lootopia/settings.py
+++ lootopia/lootopia/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework_simplejwt',
-    'app',
+    'lootopia.app',
     'corsheaders',
     'drf_yasg',
 ]
